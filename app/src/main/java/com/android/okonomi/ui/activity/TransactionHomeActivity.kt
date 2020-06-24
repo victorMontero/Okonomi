29eb97f5befb91622c39f8dcc73fea8c0cef720d
unknown <viiktoor@gmail.com>
2020-06-24 19:26:02 -0400

    home activity, floating action button and list working

---
+++ app/src/main/java/com/android/okonomi/ui/activity/TransactionHomeActivity.kt
@@ -0,0 +1,24 @@
+package com.android.okonomi.ui.activity
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.android.okonomi.R
+import kotlinx.android.synthetic.main.activity_home_transaction.*
+
+class TransactionHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_transaction)
+
+        val listTransaction = listOf("lunch - $20", "refund - $100")
+
+        val arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, listTransaction)
+
+        list_transaction_list_view.setAdapter(arrayAdapter)
+
+
+    }
+}
